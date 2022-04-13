e3003f2913a44bd99189d76c5f7dcd3fe6d12ae8
ershimen <jan.cerezo6@gmail.com>
2022-04-14 00:13:47 +0200

    Leader election works

--- node/node.go
+++ node/node.go
@@ -36,7 +36,7 @@ const (
  */
 type NodeAddr struct {
 	ip   string
-	port int16
+	port uint16
 }
 
 type Entry struct {
@@ -46,14 +46,15 @@ type Entry struct {
  * Messages
  */
 type NodeMsg struct {
-	MsgType uint8
-	Term    int
-	Entries []string
+	MsgType    uint8
+	Term       int
+	Entries    []string
+	SenderPort uint16 // porque no se sabe la ip:puerto del origen (se sabe la ip solo), el puerto puede ser otro proceso que no es el que hace net.Dial y conn.Write
 }
 
 type Event struct {
 	msg    NodeMsg
-	sender string // ip:port
+	sender string // ip (the port is in msg.SenderPort)
 }
 
 /*
@@ -66,31 +67,48 @@ type NodeLogEntry struct {
 
 /*
  * Status of the raft algorithm.
- *	peers: List of peers
- *	received_votes: Number of votes receiver in a votation
- *	term: Numer of the current term
- *	log: List of log entries
- *	status: Status of the raft algorithm. This way goroutines can end
  */
 type NodeStatus struct {
-	mutex              sync.Mutex
-	peers              []NodeAddr
-	received_votes     int8
-	voted_current_term bool
-	term               int
-	log                []NodeLogEntry
-	raftStatus         uint8
-	nodeStatus         string
-	electionTimeout    int64 // milliseconds
-	leaderHeartbeat    int64
-	eventChan          chan Event
+	port               uint16         // self port
+	mutex              sync.Mutex     // mutex for concurrent access
+	peers              []NodeAddr     // list of peers
+	received_votes     int8           // number of received votes in a election
+	voted_current_term bool           // true if has voted in current term
+	term               int            // current node term
+	log                []NodeLogEntry // list of entries
+	raftStatus         uint8          // raft status: follower | candidate | leader
+	nodeStatus         string         // node status: "dead" | "alive"
+	electionTimeout    int64          // election timeout (for followers and candidates) milliseconds
+	leaderHeartbeat    int64          // heartbeat time interval (for leaders) milliseconds
+	eventChan          chan Event     // channel to sync raft() and listener()
 }
 
 var promptPort string
 
 func main() {
 
+	// Create tcp server
+	l, err := net.Listen("tcp", "localhost:0")
+	if err != nil {
+		print("main", "Error listening")
+		os.Exit(1)
+	}
+	tcpListener, ok := l.(*net.TCPListener)
+	if !ok {
+		print("main", "Error casting to net.TCPListener")
+		os.Exit(1)
+	}
+	defer tcpListener.Close()
+
+	selfPort := uint16(tcpListener.Addr().(*net.TCPAddr).Port)
+	promptPort = strconv.Itoa(int(selfPort))
+	print("main", fmt.Sprintf("Listening on port %d", tcpListener.Addr().(*net.TCPAddr).Port))
+
+	var wg sync.WaitGroup
+
+	// initialize node status
 	status := NodeStatus{
+		port:               selfPort,
 		peers:              []NodeAddr{},
 		received_votes:     -1,
 		voted_current_term: false,
@@ -103,99 +121,170 @@ func main() {
 		eventChan:          make(chan Event, 25),
 	}
 
-	// Create tcp server
-	l, err := net.Listen("tcp", "localhost:0")
-	if err != nil {
-		fmt.Println("Error listening")
-		os.Exit(1)
+	// startup cli
+	if len(os.Args) > 1 && os.Args[1] == "--nocli" {
+		return
 	}
-	tcpListener, ok := l.(*net.TCPListener)
-	if !ok {
-		fmt.Println("Error casting to net.TCPListener")
-		os.Exit(1)
-	}
-
-	defer tcpListener.Close()
-	fmt.Println("Listening on port", tcpListener.Addr().(*net.TCPAddr).Port)
-
-	var wg sync.WaitGroup
-	wg.Add(2)
-
-	// Run response handler
-	go listener(&wg, tcpListener, &status)
-
-	// Run main algorithm
-	go raft(&wg, &status)
-
-	// Create cli
+	print("main", "Aviable commands are:")
+	print("main", "\tADD: add peer")
+	print("main", "\tPEERS: lists peers")
+	print("main", "\tSTART: start node.")
 	reader := bufio.NewReader(os.Stdin)
-	promptPort = strconv.Itoa(tcpListener.Addr().(*net.TCPAddr).Port)
-	prompt := promptPort + "> "
+	//prompt := promptPort + "> "
 	end := false
+	started := false
 	regexADD := regexp.MustCompile(`\s*ADD\s*(?P<args>[\w\W]*)`)
 	regexSTOP := regexp.MustCompile(`\s*STOP\s*(?P<args>[\w\W]*)`)
 	regexPEERS := regexp.MustCompile(`\s*PEERS\s*(?P<args>[\w\W]*)`)
+	regexSTART := regexp.MustCompile(`\s*START\s*(?P<args>[\w\W]*)`)
 
-	fmt.Println("Node console")
 	for !end {
-		fmt.Print(prompt)
+		//fmt.Print(prompt)
 		line, _, err := reader.ReadLine()
 		if err != nil {
-			fmt.Fprint(os.Stderr, "Error reading line from stdin.")
-			break
+			print("main", "Error reading line from stdin.")
+			os.Exit(1)
 		}
 		// ADD command
 		if match := regexADD.FindSubmatch(line); len(match) > 0 {
 			splittedArgs := strings.Split(string(match[1]), ":")
 			switch {
 			case len(splittedArgs) != 2:
-				fmt.Println("Error: could not read argument string, must have \"ip:port\" format.")
+				print("main", "Error: could not read argument string, must have \"ip:port\" format.")
 			default:
 				ip := splittedArgs[0]
 				port := splittedArgs[1]
 				nPort, err := strconv.Atoi(port)
-				fmt.Println("ip: \"" + ip + "\"")
-				fmt.Println("port: \"" + port + "\"")
+
+				//print("main", fmt.Sprintf("ip: \"%s\"", ip))
+				//print("main", fmt.Sprintf("port: \"%d\"", nPort))
+
 				if err != nil || len(port) == 0 {
-					fmt.Printf("Error: port value \"%s\"is not an integer.\n", port)
+					print("main", fmt.Sprintf("Error: port value \"%s\"is not an integer.\n", port))
 				}
-				addPeer(&status, ip, nPort)
-				fmt.Printf("Added peer %s:%d\n", ip, nPort)
+				status.mutex.Lock()
+				status.peers = append(status.peers, NodeAddr{ip, uint16(nPort)})
+				status.mutex.Unlock()
+				print("main", fmt.Sprintf("Added peer %s:%d", ip, nPort))
 			}
 			continue
 		}
 		// STOP command
 		if match := regexSTOP.FindSubmatch(line); len(match) > 0 {
-			fmt.Print("Stopping... ")
+			print("main", "Stopping... ")
 			status.mutex.Lock()
 			status.nodeStatus = "dead"
 			status.mutex.Unlock()
 			end = true
-			continue
+			break
 		}
 		// PEERS command
 		if match := regexPEERS.FindSubmatch(line); len(match) > 0 {
-			fmt.Println("Peers:")
+			print("main", "Peers:")
 			for _, v := range status.peers {
-				fmt.Println("\t" + v.ip + ":" + strconv.Itoa(int(v.port)))
+				print("main", fmt.Sprintf("\t%s:%d", v.ip, v.port))
 			}
-			fmt.Println()
 			continue
 		}
+		// START command
+		if match := regexSTART.FindSubmatch(line); len(match) > 0 {
+			if started {
+				print("main", "Error: node is already running")
+				continue
+			}
+			print("main", "Starting node...")
+			wg.Add(2)
+			// Run response handler
+			go listener(&wg, tcpListener, &status)
+			// Run main algorithm
+			go raft(&wg, &status)
+			started = true
+			print("main", "Started node")
+			continue
+		}
+
 		fmt.Println("Unrecognized command. Aviable commands are:")
 		fmt.Println("\tADD: add peer")
 		fmt.Println("\tPEERS: lists peers")
-		fmt.Println("\tSTOP: stop node.")
+		fmt.Println("\tSTOP: stop node")
+		fmt.Println("\tSTART: start the server")
+
 	}
-	if end { // porque podria hacer break en readstring()
-		wg.Wait()
-		fmt.Println("Stopped node.")
-	}
+
+	wg.Wait()
+	print("main", "Stopped node.")
+
+	/*
+		// Create cli
+		reader := bufio.NewReader(os.Stdin)
+		promptPort = strconv.Itoa(tcpListener.Addr().(*net.TCPAddr).Port)
+		prompt := promptPort + "> "
+		end := false
+		regexADD := regexp.MustCompile(`\s*ADD\s*(?P<args>[\w\W]*)`)
+		regexSTOP := regexp.MustCompile(`\s*STOP\s*(?P<args>[\w\W]*)`)
+		regexPEERS := regexp.MustCompile(`\s*PEERS\s*(?P<args>[\w\W]*)`)
+
+		fmt.Println("Node console")
+		for !end {
+			fmt.Print(prompt)
+			line, _, err := reader.ReadLine()
+			if err != nil {
+				fmt.Fprint(os.Stderr, "Error reading line from stdin.")
+				break
+			}
+			// ADD command
+			if match := regexADD.FindSubmatch(line); len(match) > 0 {
+				splittedArgs := strings.Split(string(match[1]), ":")
+				switch {
+				case len(splittedArgs) != 2:
+					fmt.Println("Error: could not read argument string, must have \"ip:port\" format.")
+				default:
+					ip := splittedArgs[0]
+					port := splittedArgs[1]
+					nPort, err := strconv.Atoi(port)
+					fmt.Println("ip: \"" + ip + "\"")
+					fmt.Println("port: \"" + port + "\"")
+					if err != nil || len(port) == 0 {
+						fmt.Printf("Error: port value \"%s\"is not an integer.\n", port)
+					}
+					addPeer(&status, ip, nPort)
+					fmt.Printf("Added peer %s:%d\n", ip, nPort)
+				}
+				continue
+			}
+			// STOP command
+			if match := regexSTOP.FindSubmatch(line); len(match) > 0 {
+				fmt.Print("Stopping... ")
+				status.mutex.Lock()
+				status.nodeStatus = "dead"
+				status.mutex.Unlock()
+				end = true
+				continue
+			}
+			// PEERS command
+			if match := regexPEERS.FindSubmatch(line); len(match) > 0 {
+				fmt.Println("Peers:")
+				for _, v := range status.peers {
+					fmt.Println("\t" + v.ip + ":" + strconv.Itoa(int(v.port)))
+				}
+				fmt.Println()
+				continue
+			}
+			fmt.Println("Unrecognized command. Aviable commands are:")
+			fmt.Println("\tADD: add peer")
+			fmt.Println("\tPEERS: lists peers")
+			fmt.Println("\tSTOP: stop node.")
+		}
+		if end { // porque podria hacer break en readstring()
+			wg.Wait()
+			fmt.Println("Stopped node.")
+		}
+	*/
 }
 
 func addPeer(status *NodeStatus, ip string, port int) {
 	status.mutex.Lock()
-	status.peers = append(status.peers, NodeAddr{ip, int16(port)})
+	status.peers = append(status.peers, NodeAddr{ip, uint16(port)})
 	status.mutex.Unlock()
 }
 
@@ -212,14 +301,15 @@ func listener(wg *sync.WaitGroup, l *net.TCPListener, status *NodeStatus) {
 			status.mutex.Unlock()
 			break
 		}
-		switch status.raftStatus {
-		case follower, candidate:
-			// set electionTimeout
-			timeout = time.Now().Add(time.Millisecond * time.Duration(status.electionTimeout))
-		case leader:
-			// no timeout
-			timeout = time.Time{}
+		// set electionTimeout
+		timeout = time.Now().Add(time.Millisecond * time.Duration(status.electionTimeout))
+
+		// set timeout only for follower and candidate state
+		sendTimeout := true
+		if status.raftStatus == leader {
+			sendTimeout = false
 		}
+
 		status.mutex.Unlock()
 
 		l.SetDeadline(timeout)
@@ -227,12 +317,16 @@ func listener(wg *sync.WaitGroup, l *net.TCPListener, status *NodeStatus) {
 		conn, err := l.Accept()
 		if err != nil {
 			if errors.Is(err, os.ErrDeadlineExceeded) {
-				print("listener", "Timeout accepting")
-				// timeout for follower and candidate
-				//go startElection(status)
-				status.eventChan <- Event{
-					// no hace falta rellenar los demas campos
-					msg: NodeMsg{MsgType: followerTimeout},
+				if sendTimeout {
+					print("listener", "Timeout accepting")
+					// timeout for follower and candidate
+					//go startElection(status)
+					status.eventChan <- Event{
+						// no hace falta rellenar los demas campos
+						msg: NodeMsg{MsgType: followerTimeout},
+					}
+				} else {
+					// timeout but its in leader state
 				}
 				continue
 			} else {
@@ -272,9 +366,11 @@ func responseHandler(conn net.Conn, status *NodeStatus) {
 		return
 	}
 
+	fmt.Println("sender:", conn.RemoteAddr().String())
+
 	status.eventChan <- Event{
 		msg:    msg,
-		sender: conn.RemoteAddr().String(),
+		sender: conn.RemoteAddr().(*net.TCPAddr).IP.String(),
 	}
 
 	/*
@@ -331,7 +427,14 @@ func raft(wg *sync.WaitGroup, status *NodeStatus) {
 	//electionTimer(status)
 
 	for event := range status.eventChan {
-		print("raft", fmt.Sprintf("New event: %s(%d)", msgTypeToString(event.msg.MsgType), event.msg.MsgType))
+		print("raft",
+			fmt.Sprintf(
+				"New event: %s(%d) from %s:%d",
+				msgTypeToString(event.msg.MsgType),
+				event.msg.MsgType,
+				event.sender,
+				event.msg.SenderPort),
+		)
 		status.mutex.Lock()
 		switch event.msg.MsgType {
 		case requestVote:
@@ -342,7 +445,14 @@ func raft(wg *sync.WaitGroup, status *NodeStatus) {
 					print("raft", fmt.Sprintf("Becoming follower of term %d", event.msg.Term))
 					status.voted_current_term = true
 					status.term = event.msg.Term
-					go sendMsg(NodeMsg{MsgType: grantVote, Term: event.msg.Term}, event.sender)
+					go sendMsg(
+						NodeMsg{
+							MsgType: grantVote,
+							Term:    event.msg.Term,
+						},
+						event.sender,
+						event.msg.SenderPort,
+					)
 				}
 			case candidate:
 				if event.msg.Term > status.term {
@@ -350,7 +460,14 @@ func raft(wg *sync.WaitGroup, status *NodeStatus) {
 					status.voted_current_term = true
 					status.raftStatus = follower
 					status.term = event.msg.Term
-					go sendMsg(NodeMsg{MsgType: grantVote, Term: event.msg.Term}, event.sender)
+					go sendMsg(
+						NodeMsg{
+							MsgType: grantVote,
+							Term:    event.msg.Term,
+						},
+						event.sender,
+						event.msg.SenderPort,
+					)
 				}
 			case leader:
 				// si es lider entonces ver si el term es superior
@@ -359,7 +476,14 @@ func raft(wg *sync.WaitGroup, status *NodeStatus) {
 					status.voted_current_term = true
 					status.raftStatus = follower
 					status.term = event.msg.Term
-					go sendMsg(NodeMsg{MsgType: grantVote, Term: event.msg.Term}, event.sender)
+					go sendMsg(
+						NodeMsg{
+							MsgType: grantVote,
+							Term:    event.msg.Term,
+						},
+						event.sender,
+						event.msg.SenderPort,
+					)
 				}
 			}
 
@@ -372,7 +496,12 @@ func raft(wg *sync.WaitGroup, status *NodeStatus) {
 				if event.msg.Term == status.term {
 					status.received_votes++
 					if int(status.received_votes) > (len(status.peers)+1)/2 {
-						print("raft", fmt.Sprintf("Becoming leader of term %d with %d votes", status.term, status.received_votes))
+						print(
+							"raft",
+							fmt.Sprintf("Becoming leader of term %d with %d votes",
+								status.term,
+								status.received_votes),
+						)
 						status.raftStatus = leader
 						go leaderHeartbeats(status)
 					}
@@ -439,15 +568,16 @@ func startElection(status *NodeStatus) {
 	status.received_votes = 1 // vote for itself
 
 	msg := NodeMsg{
-		MsgType: requestVote,
-		Term:    status.term,
-		Entries: nil,
+		MsgType:    requestVote,
+		Term:       status.term,
+		Entries:    nil,
+		SenderPort: status.port,
 	}
 	status.mutex.Unlock()
 
 	// send request vote to peers
 	for _, p := range status.peers {
-		go sendMsg(msg, p.ip+":"+strconv.Itoa(int(p.port)))
+		go sendMsg(msg, p.ip, p.port)
 	}
 }
 
@@ -460,16 +590,17 @@ func leaderHeartbeats(status *NodeStatus) {
 		}
 
 		msg := NodeMsg{
-			MsgType: appendEntries,
-			Term:    status.term,
-			Entries: nil,
+			MsgType:    appendEntries,
+			Term:       status.term,
+			Entries:    nil,
+			SenderPort: status.port,
 		}
 		timeout := status.leaderHeartbeat
 		status.mutex.Unlock()
 
 		// send AppendEntries to peers
 		for _, p := range status.peers {
-			go sendMsg(msg, p.ip+":"+strconv.Itoa(int(p.port)))
+			go sendMsg(msg, p.ip, p.port)
 		}
 
 		// wait for next hearbeat
@@ -478,7 +609,9 @@ func leaderHeartbeats(status *NodeStatus) {
 
 }
 
-func sendMsg(msg NodeMsg, addr string) {
+// selfPort is in msg
+func sendMsg(msg NodeMsg, ip string, port uint16) {
+	addr := ip + ":" + strconv.Itoa(int(port))
 	print(
 		"sendMsg",
 		fmt.Sprintf("Sending %s to %s",
@@ -505,6 +638,8 @@ func msgTypeToString(msgType uint8) string {
 		return "appendEntries"
 	case followerTimeout:
 		return "followerTimeout"
+	case grantVote:
+		return "grantVote"
 	default:
 		return "unknownType"
 	}
