bc70282990b473aabcf17c97898963997dad2f1b
ershimen <jan.cerezo6@gmail.com>
2022-05-04 21:00:01 +0200

    Broker server

---
+++ broker/broker.js
@@ -0,0 +1,21 @@
+var Net = require('net');
+
+const port = 12345;
+
+const server = Net.createServer();
+
+server.listen(port, function() {console.log(`Listening on port ${port}`)})
+
+server.on('connection', function(socket) {
+    console.log('New connection');
+    socket.on('data', function(buffer) {
+        console.log(`Buffer: ${buffer.toString()}`);
+        socket.write('Response');
+    });
+    socket.on('end', function() {
+        console.log('Closed connection');
+    });
+    socket.on('error', function(error) {
+        console.log(`Error: ${error}`)
+    });
+});
